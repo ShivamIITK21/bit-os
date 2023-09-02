d5065256de176c4dc9cec55a70a95980884a6873
shivam <sshivam21@iitk.ac.in>
2023-09-03 04:37:03 +0530

    refactor

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,17 @@
 #include "kernel.h"
 
 uint16_t *video_mem = (uint16_t *)(0xB8000);
+int row = 0;
+int col = 0;
 
-uint16_t encode_char(char c, char col){
-    return (col << 8) | c; 
+size_t strlen(const char *str){
+    size_t len = 0;
+    while(str[len]) len++;
+    return len;
+}
+
+uint16_t encode_char(char c, char color){
+    return (color << 8) | c; 
 }
 
 void init_terminal(){
@@ -14,14 +22,32 @@ void init_terminal(){
     }
 }
 
-void print_string(char *string, int len){
-    for(int i = 0; i < len; i++){
-        video_mem[i] = encode_char(string[i], 15);
+void put_char(int x, int y, char c, char color){
+    video_mem[x*VGA_WIDTH + y] = encode_char(c, color);
+}
+
+void print_char(char c, char color){
+    if(c == '\n'){
+        row++;
+        col = 0;
+        return;
+    }
+    put_char(row, col, c, color);
+    col++;
+    if(col == VGA_WIDTH){
+        col = 0;
+        row++;
+    }
+}
+
+void print(const char* str){
+    for(size_t i = 0; i < strlen(str); i++){
+        print_char(str[i], 15);
     }
 }
 
 void kernel_main(){
     init_terminal();
-    char *string = "Hello, World!";
-    print_string(string, 13);
+    print("Hello\n, World!");
+    print("Wassup");
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,11 +1,14 @@
 #ifndef KERNEL_H
-#include<stdint.h>
 #define KERNEL_H
+#include<stdint.h>
+#include<stddef.h>
 #define VGA_HEIGHT 20
 #define VGA_WIDTH 80
 
 uint16_t encode_char(char c, char col);
+size_t strlen(const char* str);
 void init_terminal();
+void print_string(char *string);
 void kernel_main();
 
 #endif
